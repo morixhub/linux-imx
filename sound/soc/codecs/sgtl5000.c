d39c16081d0e5024f82bde017306a6d38a9363d3
Morix Dev <morix.dev@gmail.com>
2023-05-29 13:58:16 +0200

    Removed SGTL5000 pop on audio close

--- sound/soc/codecs/sgtl5000.c
+++ sound/soc/codecs/sgtl5000.c
@@ -34,6 +34,8 @@
 #define SGTL5000_DAP_REG_OFFSET	0x0100
 #define SGTL5000_MAX_REG_OFFSET	0x013A
 
+#define SGTL5000_VAG_POWERDOWN_TIME 500 /* ms */
+
 /* default value of sgtl5000 registers */
 static const struct reg_default sgtl5000_reg_defaults[] = {
 	{ SGTL5000_CHIP_I2S_CTRL,		0x0010 },
@@ -124,16 +126,36 @@ static int mic_bias_event(struct snd_soc_dapm_widget *w,
 static int power_vag_event(struct snd_soc_dapm_widget *w,
 	struct snd_kcontrol *kcontrol, int event)
 {
+	const u32 power_mask = SGTL5000_DAC_POWERUP | SGTL5000_ADC_POWERUP;
+	const u32 mute_mask = SGTL5000_HP_MUTE | SGTL5000_LINE_OUT_MUTE;
+	static int reg = 0;
+
 	switch (event) {
-	case SND_SOC_DAPM_POST_PMU:	
+	case SND_SOC_DAPM_PRE_PMU:
+		reg = snd_soc_read(w->codec, SGTL5000_CHIP_ANA_CTRL);
+		snd_soc_update_bits(w->codec, SGTL5000_CHIP_ANA_CTRL,
+			mute_mask, mute_mask);
 		snd_soc_update_bits(w->codec, SGTL5000_CHIP_ANA_POWER,
 			SGTL5000_VAG_POWERUP, SGTL5000_VAG_POWERUP);
 		break;
 
+	case SND_SOC_DAPM_POST_PMU:
+		snd_soc_update_bits(w->codec, SGTL5000_CHIP_ANA_CTRL,
+			mute_mask, reg);
+		break;
+
 	case SND_SOC_DAPM_PRE_PMD:
-		snd_soc_update_bits(w->codec, SGTL5000_CHIP_ANA_POWER,
-			SGTL5000_VAG_POWERUP, 0);
-		msleep(400);
+		/*
+		 * Don't clear VAG_POWERUP, when both DAC and ADC are
+		 * operational to prevent inadvertently starving the
+		 * other one of them .
+		*/
+		if((snd_soc_read(w->codec, SGTL5000_CHIP_ANA_POWER) &
+				power_mask) != power_mask) {
+			snd_soc_update_bits(w->codec, SGTL5000_CHIP_ANA_POWER,
+				SGTL5000_VAG_POWERUP, 0);
+			msleep(SGTL5000_VAG_POWERDOWN_TIME);
+		}
 		break;
 	default:
 		break;
@@ -1099,7 +1121,7 @@ static int sgtl5000_probe(struct snd_soc_codec *codec)
 
 	/* enable small pop, introduce 400ms delay in turning off */
 	snd_soc_update_bits(codec, SGTL5000_CHIP_REF_CTRL,
-				SGTL5000_SMALL_POP, 1);
+				SGTL5000_SMALL_POP, SGTL5000_SMALL_POP);
 
 	/* disable short cut detector */
 	snd_soc_write(codec, SGTL5000_CHIP_SHORT_CTRL, 0);
